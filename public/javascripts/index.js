fa9864339de8116eebe3144ab18891dfeb497398
akisantos <playscooter0401@gmail.com>
2020-12-13 22:28:51 +0700

    Update giao diện, thêm phần aboutme, chức năng backandnext

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,13 @@
 console.log('loaded')
-$('#process-line').width(0);
+// $('#process-line').width(0);
 $("#lesson-name-header").hide();
 $("#lessonID").hide();
+
+// Animation
+$("#lesson-name").change(()=>{
+    $("#lesson-name").animate({opacity:0},fast,linear);
+    $("#lesson-name").animate({opacity:1},slow,linear);
+})
 
 $(".lesson").on('scroll', ()=> { 
     let holder = $(".lesson").scrollTop()*100  / ($(".lessonContent").height() - $(".lesson").height());
@@ -10,18 +16,28 @@ $(".lesson").on('scroll', ()=> {
     if($(".lesson").scrollTop() > 20) {
         let title = $("#lesson-name-header").html();
         $("#lesson-name").html(title);
+
+
     }else{
-        $("#lesson-name").html('&nbsp;');
+        $("#lesson-name").html('Tool học nhạc nho nhỏ của Akkii');
     }
 
 })
-
 
 // JSONPARSER
 const edjsParser = edjsHTML();
 $(document).ready(()=>{ 
     
+    //Khi load trang hoàn tất, kích sidebar
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+
+    //Lấy id bài rồi in ra
     let holder = $("#lessonID").html().split('"');
+
 
     $.getJSON('/javascripts/data/bai'+holder[1]+'.json',(res)=>{
 
@@ -33,6 +49,19 @@ $(document).ready(()=>{
     }).fail(function() {
         $("#lesson-name-header").html("LỖI KHÔNG TẢI ĐƯỢC BÀI, vui lòng quay lại sau");
         $("#lesson-name-header").show();
-      })
+    })
+
+    // Xử lí nút back and next
+    let numberID = parseInt(holder[1]);
+    if (numberID>1 && numberID+1<=2){
+        let back= numberID-1;
+        let next= numberID+1;
+        $("#back-button").attr("href","/lesson/"+back);
+        $("#next-button").attr("href","/lesson/"+next);
+    }else{
+        $("#back-button").attr("href","/lesson/"+0);
+        $("#next-button").attr("href","/lesson/"+1);  
+    }
+
     
 })
